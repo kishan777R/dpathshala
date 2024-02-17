b33419e37f37863ed4e8deb88aa0e1d2ac72cfc1
kishan777R <softwaredeveloperkp@gmail.com>
2024-02-17 15:44:45 +0530

    constant

--- router/contants.js
+++ router/contants.js
@@ -1,7 +1,13 @@
  
-const allconst = module.exports ={
+const allconstlocal = module.exports ={
     "dashboardpath":'http://localhost:4200/',
     "serverpath":'http://localhost:3100/',
 "websitepath":'http://localhost/DpathshalaWeb',
+"server_version_app":1
+};
+const allconst = module.exports ={
+    "dashboardpath":'http://localhost:4200/',
+    "serverpath":'http://54.80.76.39:3100/',
+"websitepath":'https://dpathshala.live',
 "server_version_app":1
 };
